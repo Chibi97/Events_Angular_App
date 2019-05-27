ddc6085ff57abd8e81e4af09ba487769fd564a8e
Olja Ivkovic <olja.ivkovic97@gmail.com>
2019-05-27 16:46:59 +0200

    change app component into events-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+        EventsAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+    bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
