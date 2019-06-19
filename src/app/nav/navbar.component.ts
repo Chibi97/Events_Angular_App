8c32608b155127438809c7f6a6dd82d069de3caa
Olja Ivkovic <olja.ivkovic97@gmail.com>
2019-06-19 23:02:30 +0200

    active link done

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-navbar',
-    templateUrl: './navbar.component.html'
+    templateUrl: './navbar.component.html',
+    styles: [
+        `li > a.active { color: #AA6373 !important; font-weight: bold !important; }`
+    ]
 })
 
 export class NavbarComponent {
